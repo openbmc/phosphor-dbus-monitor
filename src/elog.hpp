9db6339dea2796dde8b8d548d7e11c6308f2f8d6
Gunnar Mills <gmills@us.ibm.com>
2017-08-03 15:54:43 -0500

    Create new Elog class
    Created new Elog callback, which will create an error log.
    This is just the base, more to come.
    
    Change-Id: I50c12c8bff0942b5cb027e38d0cc8691e8a241b4
    Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

---
+++ src/elog.hpp
@@ -0,0 +1,70 @@
+#pragma once
+#include <phosphor-logging/log.hpp>
+#include <phosphor-logging/elog-errors.hpp>
+#include "callback.hpp"
+#include "format.hpp"
+namespace phosphor
+{
+namespace dbus
+{
+namespace monitoring
+{
+
+/** @class ElogBase
+ *  @brief Elog callback implementation.
+ *
+ *  The elog callback logs the elog and
+ *  elog metadata.
+ */
+class ElogBase : public Callback
+{
+    public:
+        ElogBase() = delete;
+        ElogBase(const ElogBase&) = delete;
+        ElogBase(ElogBase&&) = default;
+        ElogBase& operator=(const ElogBase&) = delete;
+        ElogBase& operator=(ElogBase&&) = default;
+        virtual ~ElogBase() = default;
+        ElogBase(const char* msg, const PropertyIndex& index) :
+            Callback(){}
+
+        /** @brief Callback interface implementation. */
+        void operator()() override;
+
+    private:
+        /** @brief Delegate type specific calls to subclasses. */
+        virtual void log() const = 0;
+};
+
+
+/** @class Elog
+ *  @brief C++ type specific logic for the elog callback.
+ *
+ *  @tparam T - The C++ type of the property values being traced.
+ *  @tparam Error - The log severity of the log entry.
+ */
+template <typename T>
+class Elog : public ElogBase
+{
+    public:
+        Elog() = delete;
+        Elog(const Elog&) = delete;
+        Elog(Elog&&) = default;
+        Elog& operator=(const Elog&) = delete;
+        Elog& operator=(Elog&&) = default;
+        ~Elog() = default;
+        Elog(const char* msg, const PropertyIndex& index) :
+            ElogBase() {}
+
+    private:
+        /** @brief elog interface implementation. */
+        void log() const override
+        {
+            using namespace phosphor::logging;
+            commit<T>();
+        }
+};
+
+} // namespace monitoring
+} // namespace dbus
+} // namespace phosphor
