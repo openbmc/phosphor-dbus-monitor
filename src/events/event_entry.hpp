6c6391aac8dc3de6149f682f34b5d6fced72480c
Ratan Gupta <ratagupt@in.ibm.com>
2017-10-06 21:37:01 +0530

    Implement create function in event manager
    Implement the logging event interface.
    
    Create the dbus event object based on the event type.
    
    Change-Id: Idfa9e5c43f170d904fd25f22d73e0509b1785fc9
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

---
+++ src/events/event_entry.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+#include "xyz/openbmc_project/Logging/Event/server.hpp"
+
+namespace phosphor
+{
+namespace events
+{
+
+using EntryIface = sdbusplus::server::object::object <
+                        sdbusplus::xyz::openbmc_project::Logging::server::Event >;
+
+/** @class Entry
+ *  @brief OpenBMC Event entry implementation.
+ *  @details A concrete implementation for the
+ *  xyz.openbmc_project.Event.Entry.
+ */
+class Entry : public EntryIface
+{
+    public:
+        Entry() = delete;
+        Entry(const Entry&) = delete;
+        Entry& operator=(const Entry&) = delete;
+        Entry(Entry&&) = delete;
+        Entry& operator=(Entry&&) = delete;
+        virtual ~Entry() = default;
+
+        /** @brief Constructor to put object onto bus at a dbus path.
+         *  @param[in] bus - Bus to attach to.
+         *  @param[in] path - Path to attach at.
+         *  @param[in] eventId - The event entry id.
+         *  @param[in] timestamp - timestamp when the event created.
+         *  @param[in] msg - The message of the event.
+         *  @param[in] metaData - The event metadata.
+         */
+        Entry(sdbusplus::bus::bus& bus,
+              const std::string& path,
+              uint32_t eventId,
+              uint64_t eventTimestamp,
+              std::string&& msg,
+              std::vector<std::string>&& metaData) :
+                    EntryIface(bus, path.c_str(), true)
+        {
+            id(eventId);
+            timestamp(eventTimestamp);
+            message(std::move(msg));
+            additionalData(std::move(metaData));
+            // Emit deferred signal.
+            this->emit_object_added();
+        };
+};
+
+} // namespace events
+} // namespace phosphor
