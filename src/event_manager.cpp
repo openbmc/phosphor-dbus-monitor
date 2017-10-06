949c85e555c666a6bbc37e4c817fd67dc9993500
Ratan Gupta <ratagupt@in.ibm.com>
2017-10-06 21:27:13 +0530

    Implement event manager class
    On handling of property change event, If callback is
    of type event then event manager would be asked to create
    the event dbus objects.
    
    Change-Id: I7f1d365d7f44a1cca54516e9ef555bb132d6b063
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

---
+++ src/event_manager.cpp
@@ -0,0 +1,34 @@
+/**
+ * Copyright © 2017 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "event.hpp"
+#include "event_manager.hpp"
+#include "config.h"
+
+namespace phosphor
+{
+namespace events
+{
+
+void Manager::create(const std::string& eventName,
+                     const std::string& objectPath,
+                     const std::string& propertyName,
+                     const std::string& propertyvalue)
+{
+    // TODO Implement it in later commit.
+}
+
+} // namespace events
+} // namespace phosphor
