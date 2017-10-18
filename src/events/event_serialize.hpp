964366c711eb25e5c653b4ce3db0cf5c3fe29a2b
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2017-10-18 01:41:51 -0500

    Add persistency for events
    Persist event D-bus objects using cereal
    
    Resolves openbmc/openbmc#2319
    
    Change-Id: Ifa15d944fe1d1026761f65eeb647dcbdf6afdba0
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

---
+++ src/events/event_serialize.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <experimental/filesystem>
+#include "event_entry.hpp"
+#include "config.h"
+
+namespace phosphor
+{
+namespace events
+{
+
+namespace fs = std::experimental::filesystem;
+
+/** @brief Serialize and persist event d-bus object
+ *  @param[in] event - const reference to event entry.
+ *  @param[in] eventName - Name of the event.
+ *  @param[in] dir - pathname of directory where the serialized event will
+ *                   be placed.
+ *  @return fs::path - pathname of persisted events file
+ */
+fs::path serialize(const Entry& event,
+                   const std::string eventName,
+                   const fs::path& dir = fs::path(EVENTS_PERSIST_PATH));
+
+/** @brief Deserialze a persisted event into a d-bus object
+ *  @param[in] path - pathname of persisted event file
+ *  @param[in] event - reference to event object which is the target of
+ *             deserialization.
+ *  @return bool - true if the deserialization was successful, false otherwise.
+ */
+bool deserialize(const fs::path& path, Entry& event);
+
+} // namespace events
+} // namespace phosphor
