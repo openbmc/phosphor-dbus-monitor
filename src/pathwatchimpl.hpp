72c10f76a078f6e5cf3d394a81d6d1e0e1225cd0
Marri Devender Rao <devenrao@in.ibm.com>
2018-04-12 09:18:43 -0500

    Add watch on D-Bus object paths for 'interface added' signal
    Clients specify the object paths to watch in the config yaml.
    
    Change-Id: Icb7b9bf4c072f8b8df33747c813a1f07b61de637
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

---
+++ src/pathwatchimpl.hpp
@@ -0,0 +1,68 @@
+/**
+ * @file PathWatchimpl.hpp
+ * @brief Add interfaces added watch for the specified path
+ *
+ */
+#pragma once
+
+#include <sdbusplus/message.hpp>
+#include <sdbusplus/bus/match.hpp>
+#include <vector>
+#include "callback.hpp"
+#include "data_types.hpp"
+#include "pathwatch.hpp"
+
+namespace phosphor
+{
+namespace dbus
+{
+namespace monitoring
+{
+
+template <typename DBusInterfaceType> void PathWatch<DBusInterfaceType>::start()
+{
+    if (alreadyRan)
+    {
+        return;
+    }
+    // Watch for new interfaces added on this path.
+    DBusInterfaceType::addMatch(
+        sdbusplus::bus::match::rules::interfacesAdded(objectPath),
+        [this](auto& msg)
+        // *INDENT-OFF*
+        { this->interfacesAdded(msg); });
+    // *INDENT-ON*
+
+    alreadyRan = true;
+}
+
+template <typename DBusInterfaceType>
+void PathWatch<DBusInterfaceType>::callback(Context ctx)
+{
+    // Invoke callback if present.
+    if (this->alreadyRan && this->cb)
+    {
+        (*this->cb)(ctx);
+    }
+}
+
+template <typename DBusInterfaceType>
+void PathWatch<DBusInterfaceType>::callback(Context ctx,
+                                            sdbusplus::message::message& msg)
+{
+    // Invoke callback if present.
+    if (this->alreadyRan && this->cb)
+    {
+        (*this->cb)(ctx, msg);
+    }
+}
+
+template <typename DBusInterfaceType>
+void PathWatch<DBusInterfaceType>::interfacesAdded(
+    sdbusplus::message::message& msg)
+{
+    this->callback(Context::SIGNAL, msg);
+}
+} // namespace monitoring
+} // namespace dbus
+} // namespace phosphor
