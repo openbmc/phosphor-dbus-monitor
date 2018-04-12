04525016bbf6e020d463b593160898cdf53f7c3c
Marri Devender Rao <devenrao@in.ibm.com>
2018-04-12 01:11:48 -0500

    Extend callback and watch interfaces for interfaces added signal
    At present dbus-monitor supports watch on properties. The same
    has been extended to watch on object paths for 'interfacesadded'
    signal.
    
    This is to cater to watch on error logging object so that a signal
    can be generated to event manager when new error entries are
    created.
    
    Change-Id: I105652f65e4e1c5354c934c88e4d59866540f71c
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- src/event_manager.hpp
+++ src/event_manager.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "event_entry.hpp"
-
+#include "data_types.hpp"
 #include <map>
 #include <memory>
 #include <queue>
@@ -38,7 +38,31 @@ class Manager
                 const std::string& objectPath, const std::string& propertyName,
                 const std::string& propertyValue);
 
+    /** @brief Create the D-Bus Event object.
+     *  @detail Add the objectPath, interface added as additional data
+     *          of the event object
+     *  @param[in] eventName - Name of the event.
+     *  @param[in] eventMessage - Message for the event.
+     *  @param[in] objectPath - Path of the D-Bus object.
+     *  @param[in] intrfName - Name of the interface added.
+     */
+    void create(const std::string& eventName, const std::string& eventMessage,
+                const std::string& objectPath, const std::string& interface);
+
   private:
+    using AdditionalData = std::vector<std::string>;
+
+    /** @brief Helper method to create the D-Bus Event object.
+     *  @param[in] eventName - Name of the event.
+     *  @param[in] eventMessage - Message for the event.
+     *  @param[in] objectPath - Path of the D-Bus object.
+     *  @param[in] additionalData - Additional data
+     */
+    void createHelper(const std::string& eventName,
+                      const std::string& eventMessage,
+                      const std::string& objectPath,
+                      AdditionalData&& additionalData);
+
     using EventName = std::string;
     /** @brief Queue of events */
     using EventQueue = std::queue<std::unique_ptr<Entry>>;
