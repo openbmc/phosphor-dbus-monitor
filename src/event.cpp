eec159f9175148fb5176faea7f510bb94dc738fc
Marri Devender Rao <devenrao@in.ibm.com>
2018-04-17 10:58:18 -0500

    Send SNMP trap by reading error entry properties
    Resolves openbmc/openbmc#3059
    
    Change-Id: I934ff3330f383f51de45440a79e8303bacfec5ab
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- src/event.cpp
+++ src/event.cpp
@@ -1,10 +1,16 @@
 #include "event.hpp"
+#include "snmp_notification.hpp"
+#include <xyz/openbmc_project/Logging/Entry/server.hpp>
 namespace phosphor
 {
 namespace dbus
 {
 namespace monitoring
 {
+using namespace phosphor::snmp;
+using namespace sdbusplus::xyz::openbmc_project::Logging::server;
+static constexpr auto entry = "xyz.openbmc_project.Logging.Entry";
+
 void SNMPEvent::operator()(Context ctx)
 {
 }
@@ -13,9 +19,27 @@ void SNMPEvent::operator()(Context ctx, sdbusplus::message::message& msg)
 {
     if (ctx == Context::SIGNAL)
     {
-        // TODO: parsing and invoking SNMP will be taken in new patches
-        // TODO openbmc/openbmc#3059
-        // Send out SNMP traps corresponding to BMC error logs
+        if (type == "ErrorNotification")
+        {
+            sdbusplus::message::object_path path;
+            msg.read(path);
+            PathInterfacesAdded intfs;
+            msg.read(intfs);
+            auto it = intfs.find(entry);
+            if (it != intfs.end())
+            {
+                auto& propMap = it->second;
+                uint32_t errorID = propMap.at("Id").get<uint32_t>();
+                uint64_t timestamp = propMap.at("Timestamp").get<uint64_t>();
+                std::string sev = propMap.at("Severity").get<std::string>();
+                uint8_t isev = static_cast<uint8_t>(
+                    Entry::convertLevelFromString(sev));
+                std::string message = propMap.at("Message").get<std::string>();
+                bool resolved = propMap.at("Resolved").get<bool>();
+                sendTrap<ErrorNotification>(errorID, timestamp, isev,
+                    message, resolved);
+            }
+        }
     }
 }
 } // namespace monitoring
